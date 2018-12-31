351522765f3a5319e2202d055a84156d7d6b84ca
KamoKamo <tamutamuphone@gmail.com>
2018-12-31 23:39:49 +0900

    遠隔操作のテスト（円周率のモンテカルロ法）

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-##
+#%%
 print("hello,world")
+
+#%%
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = np.array([])
+r = 0
+for i in range(100000):
+    a , b = np.random.rand() , np.random.rand()
+    if a**2+b**2<=1 :
+        r = r + 1
+    x = np.insert(x,len(x),(r/(i+1))*4)
+
+ax = plt.subplot()
+ax.plot(x)
+ax.set_ylim(3.13,3.15)
+plt.show()
+
+#%%
+!dir
