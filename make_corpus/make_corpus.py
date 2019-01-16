ea72489900dd4cdd226e5c0bea2e06227391808c
KamoKamo <tamutamuphone@gmail.com>
2019-01-16 22:28:21 +0900

    コーパス生成スクリプト（とてもおそい）

--- make_corpus/make_corpus.py
+++ make_corpus/make_corpus.py
@@ -2,6 +2,7 @@
 from logging import getLogger, StreamHandler, DEBUG, NullHandler, Formatter, INFO, basicConfig
 import re
 import os
+from pathlib import Path
 
 from lxml import etree
 from gensim.models import Word2Vec
@@ -20,9 +21,13 @@ def close_connection(conn,cur):
     cur.close()
     conn.close()
 
-def check_category(page_id):
-    #stab
-    return True
+def check_category(cur, page_id,*,scheme):
+        cur.execute("""
+        select exists ( select category from wiki_temp.{0} where category in 
+        (select cl_to from `jawiki-page`.categorylinks where cl_from = {1}) );
+       """.format(scheme,page_id))
+        result = cur.fetchall()
+        return result[0][0]
 
 #%%
 def train_corpus(sentences,model=None):
@@ -38,7 +43,7 @@ def train_corpus(sentences,model=None):
     return model
 
 #%%
-def get_sentences_xml(path,*,logger=None):
+def get_sentences_xml(path,cur,t_analyzer,*,logger=None):
     __logger = getLogger(__name__)
     __logger.addHandler(NullHandler())
     logger = logger or __logger
@@ -48,14 +53,13 @@ def get_sentences_xml(path,*,logger=None):
     xml_string = re.sub(r"</??[^(doc)(docs)]+?(\s.+?)??>","",xml_string)
     root = etree.XML(xml_string,etree.XMLParser(recover=True))
     sentences = []
-    t_analyzer = get_analyzer()
     for doc in root:
         page_id = doc.attrib["id"]
-        if check_category(page_id):
+        logger.debug("{0} : {1}".format(check_category(cur,page_id,scheme="politics"),doc.attrib["title"]))
+        if check_category(cur,page_id,scheme="politics"):
             logger.info("Now: {0}:{1}".format(page_id,doc.attrib["title"]))
             lines = doc.text.split("\n")
             for l in lines:
-                logger.debug(l)
                 sentences.append(get_wakati(l,t_analyzer))
     return sentences
 
@@ -79,6 +83,7 @@ def get_analyzer():
 #%%
 if __name__=="__main__":
     logger = getLogger()
+    logger.setLevel(INFO)
     handler = StreamHandler()
     formatter = Formatter("%(asctime)s : %(levelname)s : %(message)s")
     handler.setFormatter(formatter)
@@ -86,11 +91,23 @@ if __name__=="__main__":
     logger.addHandler(handler)
     logger.propagate = False
 
-    conn , cur = setup_connector()
-    cur.execute("select * from wiki_temp.japan_plitics limit 10")
-    for row in cur.fetchall():
-        print(row[0],row[1])
+    conn, cur = setup_connector()
+    wiki_dir = Path("E:\wiki")
+    model = None
+    t_analyzer = get_analyzer()
+    for subdir in wiki_dir.glob("*"):
+        if subdir.is_dir():
+            for xml in subdir.glob("*"):
+                if xml.is_dir():
+                    continue
+                if model is None:
+                    model = train_corpus(
+                        get_sentences_xml(str(xml),cur,t_analyzer,logger=logger))
+                else:
+                    model = train_corpus(
+                        get_sentences_xml(str(xml),cur,t_analyzer,logger=logger),model)
+                model.save("make_corpus\politics.model")
+                
+    #model = train_corpus(get_sentences_xml("E:\\wiki\\AA\\wiki_00",cur,logger=logger))
+    #model.save("debug2.bin")
     close_connection(conn,cur)
-    
-    #model = train_corpus(get_sentences_xml("E:\\wiki\\AA\\wiki_00"))
-    #model.save("debug.bin")
