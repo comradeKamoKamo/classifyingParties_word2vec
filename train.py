550d98119cf2979b65c3a4e2c5436ce97101108b
KamoKamo <tamutamuphone@gmail.com>
2019-01-25 23:40:26 +0900

    Ready to expriments

--- train.py
+++ train.py
@@ -15,127 +15,132 @@ from sklearn.metrics import precision_recall_curve,auc,roc_curve,confusion_matri
 from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 
-#%%
-def main():
-    data_dict = load_data()
-    X, y, politicians = data_to_xy(data_dict)
-    n_classes = len(politicians)
-    y = utils.np_utils.to_categorical(y,n_classes)
-    X_train , X_test , y_train ,y_test = train_test_split(X,y,test_size=0.3)
-    model = build_model(n_classes)
-    train(X_train,y_train,X_test,y_test,model)
-    test(model,X_test,y_test,politicians)
-#%%
-def build_model(n_classes):
-    model = Sequential()
-    model.add(Dense(128, input_shape=(200,), activation="relu"))
-    model.add(Dense(64,activation="relu"))
-    model.add(Dense(32,activation="relu"))
-    model.add(Dense(16,activation="relu"))
-    model.add(Dense(n_classes,activation="softmax"))    
-    model.summary()
-    json_string = model.to_json()
-    Path("model.json").open("w").write(json_string)
-    return model
+class Train:
 
-def train(X_train,y_train,X_test,y_test,model):
-    # 目的関数にSGD、誤差関数にloglessを用いる
-    model.compile(optimizer='sgd',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-    es_cb = EarlyStopping()
-    model.fit(X_train,y_train,
-        validation_data=(X_test,y_test),epochs=256,callbacks=[es_cb])
-    model.save_weights("model.hdf5")
-    r = model.evaluate(X_test,y_test)
-    print(r)
-    return model
+    def get_politicians(self):
+        data_dict = self.load_data()
+        return list(data_dict.keys())
 
-def test(model,X_test,y_test,politicians):
-    c_acc = 0
-    b_acc = 0
-    n_classes = len(politicians)
+    def main(self,extext=""):
+        data_dict = self.load_data()
+        X, y, politicians = self.data_to_xy(data_dict)
+        n_classes = len(politicians)
+        X_train , X_test , y_train ,y_test = train_test_split(X,y,test_size=0.3)
+        y_test_raw = copy.copy(y_test)
+        y_train = utils.np_utils.to_categorical(y_train, n_classes)
+        y_test = utils.np_utils.to_categorical(y_test, n_classes)
+        model = self.build_model(n_classes)
+        self.train(X_train,y_train,X_test,y_test,model,extext)
+        self.test(model,X_test,y_test_raw,politicians,extext)
 
-    raw_preds = model.predict(X_test)
-    preds = []
-    for raw_pred in raw_preds:
-        preds.append(np.where(raw_pred == max(raw_pred))[0][0])
-    y_test = np.array(y_test)
-    preds = np.array(preds)
+    def build_model(self,n_classes):
+        model = Sequential()
+        model.add(Dense(128, input_shape=(200,), activation="relu"))
+        model.add(Dense(64,activation="relu"))
+        model.add(Dense(32,activation="relu"))
+        model.add(Dense(16,activation="relu"))
+        model.add(Dense(n_classes,activation="softmax"))    
+        model.summary()
+        json_string = model.to_json()
+        Path("model.json").open("w").write(json_string)
+        return model
 
-    cm = confusion_matrix(y_test,preds)
-    cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
-    np.save("cm.npy",cm)
-    
-    fig = plt.figure()
-    ax = plt.subplot()
-    cax = ax.matshow(cm, interpolation="nearest", cmap="autumn_r")
-    fig.colorbar(cax)
-    ax.set_xticklabels([""]+politicians)
-    ax.set_yticklabels([""]+politicians)
-    plt.title("Normalized Confusion Matrix")
-    plt.xlabel("Predicted class")
-    plt.ylabel("True class")
-    plt.show()
+    def train(self,X_train,y_train,X_test,y_test,model,extext=""):
+        # 目的関数にSGD、誤差関数にloglessを用いる
+        model.compile(optimizer='sgd',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+        es_cb = EarlyStopping()
+        model.fit(X_train,y_train,
+            validation_data=(X_test,y_test),epochs=256,callbacks=[es_cb])
+        model.save_weights("model{0}.hdf5".format(extext))
+        r = model.evaluate(X_test,y_test)
+        print(r)
+        return model
 
-    labels = []
-    for i in range(len(parties)):
-        labels.append(i)
-    y_test = label_binarize(y_test,classes=labels)
-    preds = label_binarize(preds,classes=labels)
-    precision, recall , _ = precision_recall_curve(y_test.ravel(),preds.ravel())
-    prc_auc = auc(recall,precision)
-    np.save("precision.npy",precision)
-    np.save("recall.npy",recall)
+    def test(self,model,X_test,y_test,politicians,extext=""):
+        c_acc = 0
+        n_classes = len(politicians)
 
-    
-    plt.figure()
-    plt.step(recall, precision, color="b", alpha=0.2,where="post")
-    plt.fill_between(recall, precision, step="post", alpha=0.2, color="b")
-    plt.xlabel("Recall")
-    plt.ylabel("Precision")
-    plt.title("P/R (Micro Average) AUC={0}".format(prc_auc))
-    plt.show()
-    
+        raw_preds = model.predict(X_test)
+        preds = []
+        for raw_pred in raw_preds:
+            preds.append(np.where(raw_pred == max(raw_pred))[0][0])
+        y_test = np.array(y_test)
+        preds = np.array(preds)
 
-    fpr , tpr , _ = roc_curve(y_test.ravel(),preds.ravel())
-    roc_auc = auc(fpr,tpr)
-    np.save("fpr.npy",fpr)
-    np.save("tpr.npy",tpr)
+        cm = confusion_matrix(y_test,preds)
+        cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+        np.save("data/cm{0}.npy".format(extext),cm)
+        
+        fig = plt.figure()
+        ax = plt.subplot()
+        cax = ax.matshow(cm, interpolation="nearest", cmap="autumn_r")
+        fig.colorbar(cax)
+        ax.set_xticklabels([""]+politicians)
+        ax.set_yticklabels([""]+politicians)
+        plt.title("Normalized Confusion Matrix")
+        plt.xlabel("Predicted class")
+        plt.ylabel("True class")
+        plt.show()
 
-    
-    plt.figure()
-    plt.step(fpr, tpr, color="r", alpha=0.2,where="post")
-    plt.fill_between(fpr,tpr, step="post", alpha=0.2, color="r")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.plot([0,1],[0,1],linestyle="dashed",color="pink")
-    plt.title("ROC (Micro Average) AUC={0}".format(roc_auc))
-    plt.show()
-    
+        labels = []
+        for i in range(n_classes):
+            labels.append(i)
+        y_test = label_binarize(y_test,classes=labels)
+        preds = label_binarize(preds,classes=labels)
+        precision, recall , _ = precision_recall_curve(y_test.ravel(),preds.ravel())
+        prc_auc = auc(recall,precision)
+        np.save("data/precision{0}.npy".format(extext),precision)
+        np.save("data/recall{0}.npy".format(extext),recall)
 
-    return c_acc 
+        
+        plt.figure()
+        plt.step(recall, precision, color="b", alpha=0.2,where="post")
+        plt.fill_between(recall, precision, step="post", alpha=0.2, color="b")
+        plt.xlabel("Recall")
+        plt.ylabel("Precision")
+        plt.title("P/R (Micro Average) AUC={0}".format(prc_auc))
+        plt.show()
+        
 
-#%%
-def load_data():
-    data = dict()
-    for npy in Path("Data").glob("*.npy"):
-        name = npy.stem
-        data[name] = np.load(str(npy))
-    return data
+        fpr , tpr , _ = roc_curve(y_test.ravel(),preds.ravel())
+        roc_auc = auc(fpr,tpr)
+        np.save("data/fpr{0}.npy".format(extext),fpr)
+        np.save("data/tpr{0}.npy".format(extext),tpr)
 
-def data_to_xy(dict_data):
-    politicians = []
-    x, y = [], []
-    i = 0
-    for key , data in zip(dict_data.keys(), dict_data.values()) :
-        politicians.append(key)
-        for vector in data:
-            x.append(vector)
-            y.append(i)
-        i += 1
-    return np.asarray(x), np.asarray(y), politicians
+        
+        plt.figure()
+        plt.step(fpr, tpr, color="r", alpha=0.2,where="post")
+        plt.fill_between(fpr,tpr, step="post", alpha=0.2, color="r")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.plot([0,1],[0,1],linestyle="dashed",color="pink")
+        plt.title("ROC (Micro Average) AUC={0}".format(roc_auc))
+        plt.show()
+        
 
-#%%
+        return c_acc 
+
+    def load_data(self):
+        data = dict()
+        for npy in Path("politicians").glob("*.npy"):
+            name = npy.stem
+            data[name] = np.load(str(npy))
+        return data
+
+    def data_to_xy(self,dict_data):
+        politicians = []
+        x, y = [], []
+        i = 0
+        for key , data in zip(dict_data.keys(), dict_data.values()) :
+            politicians.append(key)
+            for vector in data:
+                x.append(vector)
+                y.append(i)
+            i += 1
+        return np.asarray(x), np.asarray(y), politicians
+
 if __name__ == "__main__" :
-    main()    
+    train = Train()
+    train.main()
